cdce7548e8ff9849fd7d88d6950f01cc897636df
amanda1433235 <128075432+amanda1433235@users.noreply.github.com>
2023-05-11 16:20:25 -0300

    Add files via upload

---
+++ src/navBar-popup/script.js
@@ -0,0 +1,9 @@
+document.getElementById("pacienteBtn").addEventListener("click" , function(){
+    document.querySelector(".popup").style.display = "flex";
+});
+
+
+
+document.querySelector(".close").addEventListener("click" , function(){
+    document.querySelector(".popup").style.display = "none";
+});
